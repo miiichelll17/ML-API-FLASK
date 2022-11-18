3a2c24fccbc88ea6a638d9762a1668786aab833f
Michel <michel.2018@students.unila.ac.id>
2022-11-18 22:32:31 +0700

    nambahin pencatatan tapi ngasal

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 import random
 from flask_bcrypt import Bcrypt
+import pandas as pd
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -64,7 +66,8 @@ def register():
         cur = mysql.connection.cursor()
         userDetails = request.form
         username = userDetails['username']
-        passwordstring = userDetails['password']
+        passwordstring = userDetails['password'].encode('utf-8')
+        hash_password = bcrypt.hashpw(passwordstring, bcrypt.gensalt())
 
         validateUsername = cur.execute(
             "SELECT username FROM users WHERE username = %s", [username])
@@ -74,13 +77,13 @@ def register():
         # hashed_password = Bcrypt.generate_password_hash('qwerty',passwordstring)
 
         cur.execute("INSERT INTO users(username, password) VALUES(%s, %s)",
-                    (username, passwordstring))
+                    (username, hash_password))
         mysql.connection.commit()
         cur.close()
         return 'success'
 
 
-@app.route('/login', methods=['GET', 'POST'])_
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -91,18 +94,14 @@ def login():
         curl.close()
 
         if user is not None and len(user) > 0:
-            if bcrypt.hashpw(password, user['password'].encode(utf-8)) == user['password'].encode(utf-8):
-                session['name'] = user['name']
-                session['username'] = user['username']
-                return redirect(url_for('home'))
+            if bcrypt.hashpw(password, user[2].encode('utf-8')) == user[2].encode('utf-8'):
+                return "Success"
             else:
-                flash("Gagal, username dan password tidak cocok")
-                return redirect(url_for('login'))
+                return "Gagal, username dan password tidak cocok"
         else:
-            flash("Username tidak ditemukan")
-            return redirect(url_for('login'))
+            return "Username tidak ditemukan"
     else:
-        return render_template(url_for('login.html'))
+        return "Method undefined"
 
 
 # @app.route("/penyakit", methods=['POST'])
@@ -135,123 +134,280 @@ def login():
 #             "data": result
 #         }
 
-@app.route("/penyakit/<int:id_penyakit>", methods=['PUT'])
-def update(id_penyakit):
-    if request.method == 'PUT':
-        cur = mysql.connection.cursor()
-        searchpenyakit = cur.execute(
-            "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
-        row_headers = [x[0] for x in cur.description]
-        if (searchpenyakit > 0):
-            penyakit = cur.fetchall()
-            json_data = []
-            for result in penyakit:
-                json_data.append(dict(zip(row_headers, result)))
-            # return jsonify(json_data)
-            if (request.files['image'].filename == ''):
-                fileName = json_data[0]['image']
+# @app.route("/penyakit/<int:id_penyakit>", methods=['PUT'])
+# def update(id_penyakit):
+#     if request.method == 'PUT':
+#         cur = mysql.connection.cursor()
+#         searchpenyakit = cur.execute(
+#             "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
+#         row_headers = [x[0] for x in cur.description]
+#         if (searchpenyakit > 0):
+#             penyakit = cur.fetchall()
+#             json_data = []
+#             for result in penyakit:
+#                 json_data.append(dict(zip(row_headers, result)))
+#             # return jsonify(json_data)
+#             if (request.files['image'].filename == ''):
+#                 fileName = json_data[0]['image']
+#             else:
+#                 img = request.files['image']
+#                 os.remove("./static/" + json_data[0]['image'])
+#                 splitfile = os.path.splitext(img.filename)
+#                 fileName = splitfile[0] + \
+#                     str(random.randint(1, 1000)) + splitfile[1]
+#                 img.save("./static/" + fileName)
+
+#             penyakitDetails = request.form
+#             latitude = penyakitDetails['latitude']
+#             longitude = penyakitDetails['longitude']
+#             createdAt = json_data[0]['createdAt']
+#             updatedAt = datetime.now()
+
+#             url = os.path.join('static/', fileName)
+#             img_path = url
+
+#             p = predict_image(img_path)
+#             print(p)
+#             result = dictionary(p)
+
+#             cur = mysql.connection.cursor()
+#             cur.execute("UPDATE penyakits SET indikasi=%s, latitude=%s, longitude=%s, createdAt=%s, updatedAt=%s, image=%s, url=%s WHERE id_penyakit=%s",
+#                         (result['result'], latitude, longitude, createdAt, updatedAt, fileName, url, id_penyakit))
+#             mysql.connection.commit()
+#             cur.close()
+#             return {
+#                 "status": 204,
+#                 "message": "Penyakit berhasil perbarui",
+#                 "data": result
+#             }
+
+#         else:
+#             return "penyakit not found"
+
+
+# @app.route('/penyakit', methods=['GET'])
+# def get_penyakit():
+#     cur = mysql.connection.cursor()
+#     result = cur.execute("SELECT * FROM penyakits AS result")
+#     row_headers = [x[0] for x in cur.description]
+#     if result > 0:
+#         penyakitDetails = cur.fetchall()
+#         json_data = []
+#         for result in penyakitDetails:
+#             json_data.append(dict(zip(row_headers, result)))
+#         return jsonify(json_data)
+#         # return {
+#         #     "status": 200,
+#         #     "message": "Penyakit ditemukan",
+#         #     "data": penyakitDetails
+#         # }
+#     return result
+
+
+# @app.route('/penyakit/<int:id_penyakit>', methods=['GET'])
+# def get_penyakit_by_id(id_penyakit):
+#     cur = mysql.connection.cursor()
+#     result = cur.execute(
+#         "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
+#     row_headers = [x[0] for x in cur.description]
+#     if result > 0:
+#         penyakitDetails = cur.fetchall()
+#         json_data = []
+#         for result in penyakitDetails:
+#             json_data.append(dict(zip(row_headers, result)))
+
+#     return jsonify(json_data)
+#     # return {
+#     #         "status": 200,
+#     #         "message": "Penyakit ditemukan",
+#     #         "data": data
+#     #     }
+
+
+# @app.route('/penyakit/<int:id_penyakit>', methods=['DELETE'])
+# def delete(id_penyakit):
+#     cur = mysql.connection.cursor()
+#     searchpenyakit = cur.execute(
+#         "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
+#     row_headers = [x[0] for x in cur.description]
+#     if (searchpenyakit > 0):
+#         penyakit = cur.fetchall()
+#         json_data = []
+#         for result in penyakit:
+#             json_data.append(dict(zip(row_headers, result)))
+#     else:
+#         return {
+#             "status": 400,
+#             "message": "Penyakit tidak ditemukan"
+#         }
+
+#     os.remove("./static/" + json_data[0]['image'])
+#     cur = mysql.connection.cursor()
+#     cur.execute("DELETE FROM penyakits WHERE id_penyakit={}".format(id_penyakit))
+#     mysql.connection.commit()
+#     cur.close()
+
+#     return {
+#         "status": 200,
+#         "message": "Penyakit dihapus",
+#     }
+
+
+@app.route('/pencatatan/insert', methods=['POST'])
+def insert():
+
+       try:
+            jeniskopi = request.form['addname']
+            harga = request.form['addprice']
+            jumlah = request.form['addquantity']
+
+            if jeniskopi == "":
+                return render_template('index.html')
+            elif harga == "":
+                return render_template('index.html')
+            elif jumlah == "":
+                return render_template('index.html')
             else:
-                img = request.files['image']
-                os.remove("./static/" + json_data[0]['image'])
-                splitfile = os.path.splitext(img.filename)
-                fileName = splitfile[0] + \
-                    str(random.randint(1, 1000)) + splitfile[1]
-                img.save("./static/" + fileName)
 
-            penyakitDetails = request.form
-            latitude = penyakitDetails['latitude']
-            longitude = penyakitDetails['longitude']
-            createdAt = json_data[0]['createdAt']
-            updatedAt = datetime.now()
+               if request.method == 'POST':
+                   mydb = pymysql.connect(
+                       user='root', password='root', host='localhost', database="deteksi_kopi")
+                    mycursor = mydb.cursor()
+                    if request.method == 'POST':
+                        mycursor.execute(
+                            "SELECT COUNT(1) FROM hasil_panen WHERE product_name = %s;", [jeniskopi])
+                         if mycursor.fetchone()[0]:
+                            flash(jeniskopi + ' is already on the list')
+                            return render_template('index.html')
+                         else:
+                            mycursor.execute(
+                                "INSERT INTO hasil_panen(product_name, price ,quantity) Values(%s,%s,%s)", (jeniskopi, harga, jumlah))
+                            mydb.commit()
+                            mycursor.close()
+                            mydb.close()
+                            flash(jeniskopi + ', ' + harga + ', ' +
+                                  jumlah + ', Successfully saved!')
+                            return render_template('index.html')
+                    else:
+                        return render_template('index.html')
+        except Exception as e:
+            flash(e)
+            return render_template('index.html')
 
-            url = os.path.join('static/', fileName)
-            img_path = url
 
-            p = predict_image(img_path)
-            print(p)
-            result = dictionary(p)
 
-            cur = mysql.connection.cursor()
-            cur.execute("UPDATE penyakits SET indikasi=%s, latitude=%s, longitude=%s, createdAt=%s, updatedAt=%s, image=%s, url=%s WHERE id_penyakit=%s",
-                        (result['result'], latitude, longitude, createdAt, updatedAt, fileName, url, id_penyakit))
-            mysql.connection.commit()
-            cur.close()
-            return {
-                "status": 204,
-                "message": "Penyakit berhasil perbarui",
-                "data": result
-            }
+@app.route('/pencatatan/search', methods=['POST'])
+def search():
 
+    try:
+        jeniskopi = request.form['carikopi']
+        if jeniskopi == "":
+            flash('Harap isi kolom pencarian ')
+            return render_template('index.html')
         else:
-            return "penyakit not found"
+            if request.method == 'POST':
+                mydb = pymysql.connect(
+                    user='root', password='root', host='localhost', database='deteksi_kopi')
+                mycursor = mydb.cursor()
+                if request.method == 'POST':
+                    mycursor.execute(
+                        "SELECT COUNT(1) FROM hasil_panen WHERE product_name = %s;", [jeniskopi])
+                    if mycursor.fetchone()[0]:
+                        mycursor.execute(
+                            "SELECT * FROM hasil_panen WHERE product_name = %s;", [jeniskopi])
+                        prod = mycursor.fetchall()
+                        flash(jeniskopi + ' Ditemukan!')
+                        return render_template('index.html', jeniskopis=prod)
+                    else:
+                        flash('Tidak ada jenis kopi tersebut ' + jeniskopi)
+                    return render_template('index.html')
+                else:
+                    flash('Tidak ada jenis kopi tersebut ' + jeniskopi)
+                    return render_template('index.html')
+            else:
+                return render_template('index.html')
+
+    except Exception as e:
+        flash(e)
+        return render_template('index.html')
 
 
-@app.route('/penyakit', methods=['GET'])
-def get_penyakit():
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * FROM penyakits AS result")
-    row_headers = [x[0] for x in cur.description]
-    if result > 0:
-        penyakitDetails = cur.fetchall()
-        json_data = []
-        for result in penyakitDetails:
-            json_data.append(dict(zip(row_headers, result)))
-        return jsonify(json_data)
-        # return {
-        #     "status": 200,
-        #     "message": "Penyakit ditemukan",
-        #     "data": penyakitDetails
-        # }
-    return result
+@app.route('/pencatatan/delete', methods=['POST'])
+def delete():
+    try:
+        namee = request.form['prodsname']
+        if namee == "":
+            return render_template('index.html')
+        else:
+            if request.method == 'POST':
+                mydb = pymysql.connect(
+                    user='root', password='root', host='localhost', database='deteksi_kopi')
+                mycursor = mydb.cursor()
+                if request.method == 'POST':
+                    mycursor.execute(
+                        "SELECT COUNT(1) FROM hasil_panen WHERE product_name = %s;", [namee])
+                    if mycursor.fetchone()[0]:
+                        mycursor.execute(
+                            "DELETE FROM hasil_panen WHERE product_name = %s;", [namee])
+                        mydb.commit()
+                        mycursor.close()
+                        mydb.close()
+                        flash('Berhasil Menghapus!')
+                        return render_template('index.html')
+                    else:
+                        flash(
+                            'Product ' + request.form['prodsname'] + 'Tidak ada dalam list')
+        return render_template('index.html')
+    except Exception as e:
+        flash(e)
+        return render_template('index.html')
 
 
-@app.route('/penyakit/<int:id_penyakit>', methods=['GET'])
-def get_penyakit_by_id(id_penyakit):
-    cur = mysql.connection.cursor()
-    result = cur.execute(
-        "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
-    row_headers = [x[0] for x in cur.description]
-    if result > 0:
-        penyakitDetails = cur.fetchall()
-        json_data = []
-        for result in penyakitDetails:
-            json_data.append(dict(zip(row_headers, result)))
-
-    return jsonify(json_data)
-    # return {
-    #         "status": 200,
-    #         "message": "Penyakit ditemukan",
-    #         "data": data
-    #     }
-
-
-@app.route('/penyakit/<int:id_penyakit>', methods=['DELETE'])
-def delete(id_penyakit):
-    cur = mysql.connection.cursor()
-    searchpenyakit = cur.execute(
-        "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
-    row_headers = [x[0] for x in cur.description]
-    if (searchpenyakit > 0):
-        penyakit = cur.fetchall()
-        json_data = []
-        for result in penyakit:
-            json_data.append(dict(zip(row_headers, result)))
-    else:
-        return {
-            "status": 400,
-            "message": "Penyakit tidak ditemukan"
-        }
-
-    os.remove("./static/" + json_data[0]['image'])
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM penyakits WHERE id_penyakit={}".format(id_penyakit))
-    mysql.connection.commit()
-    cur.close()
-
-    return {
-        "status": 200,
-        "message": "Penyakit dihapus",
-    }
+@app.route('/pencatatan/update', methods=['POST'])
+def update():
+    try:
+       name_prod = request.form['namess']
+       price = request.form['pricee']
+       quantt = request.form['quantityy']
+       if name_prod == "":
+           return render_template('index.html')
+       elif price == "":
+           return render_template('index.html')
+       elif quantt == "":
+           return render_template('index.html')
+       else:
+           if request.method == 'POST':
+               mydb = pymysql.connect(
+                   user='root', password='root', host='localhost', database='deteksi_kopi')
+               mycursor = mydb.cursor()
+               if request.method == 'POST':
+                   mycursor.execute(
+                       "SELECT COUNT(1) FROM hasil_panen WHERE product_name = %s;", [name_prod])
+                   if mycursor.fetchone()[0]:
+                       mycursor.execute(
+                           "SELECT * FROM hasil_panen WHERE product_name = %s;", [name_prod])
+                       produ = mycursor.fetchall()
+                       flash(name_prod + ' Ditemukan!')
+                       for row in produ:
+                           b = row[3]
+                           if int(b)+int(quantt) < 0:
+                               flash('Tidak cukup')
+                               return render_template('index.html')
+                           else:
+                               sum = int(b) + int(quantt)
+                               mycursor.execute("UPDATE hasil_panen SET price='" + price + "' , quantity='" + str(
+                                   sum) + "' WHERE product_name='" + name_prod + "'")
+                               mydb.commit()
+                               mycursor.close()
+                               mydb.close()
+                               flash('Berhasil Diupdate')
+                               return render_template('index.html')
+                   else:
+                       flash('Tidak ada jenis kopi tersebut ' + name_prod)
+                   return render_template('index.html')
+    except Exception as e:
+        flash(e)
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
