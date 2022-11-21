42db9790992a22f0f62e616bec38ab0ddf85c4a4
Michel <michel.2018@students.unila.ac.id>
2022-11-21 23:15:33 +0700

    ga nambahin apa apa soalnya udah nambahin time gbs

--- app.py
+++ app.py
@@ -17,36 +17,6 @@ from PIL import Image
 
 app = Flask(__name__)
 
-# model = keras.models.load_model('model_v1.h5')
-
-# class NumpyArrayEncoder(JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj, np.ndarray):
-#             return obj.tolist()
-#         return JSONEncoder.default(self, obj)
-
-# def predict_image(path):
-#     img = image.load_img(path, target_size=(224, 224))
-#     x = image.img_to_array(img)
-#     x = np.expand_dims(x, axis=0)
-#     images = np.vstack([x])
-#     classes = model.predict(images, batch_size=32)
-#     return classes
-
-# def dictionary(result):
-#     if result[0][0] == 1:
-#         return {"result":"narrow brown spot"}
-#     if result[0][1] == 1:
-#         return {"result":"brown spot"}
-#     if result[0][2] == 1:
-#         return {"result":"healthy"}
-#     if result[0][3] == 1:
-#         return {"result":"backterial leaf blight"}
-#     if result[0][4] == 1:
-#         return {"result":"leaf blast"}
-#     if result[0][5] == 1:
-#         return {"result":"leaf scald"}
-
 db = yaml.safe_load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
@@ -106,42 +76,44 @@ def login():
 
 @app.route('/pencatatan/insert', methods=['POST'])
 def insert():
-        try:
-            jeniskopi = request.form['addname']
-            harga = request.form['addprice']
-            jumlah = request.form['addquantity']
+    # try:
+    jeniskopi = request.form['addname']
+    harga = request.form['addprice']
+    jumlah = request.form['addquantity']
+    # waktu = request.form['addtime']
 
-            if jeniskopi == "":
-                return "Jenis Kopi tidak boleh kosong"
-            elif harga == "":
-                return "Harga tidak boleh kosong"
-            elif jumlah == "":
-                return "Jumlah tidak boleh kosong"
+    if jeniskopi == "":
+        return "Jenis Kopi tidak boleh kosong"
+    elif harga == "":
+        return "Harga tidak boleh kosong"
+    elif jumlah == "":
+        return "Jumlah tidak boleh kosong"
+    # elif waktu == "":
+    #     return "Waktu tidak boleh kosong"
 
-            
-            if request.method == 'POST':
-                mycursor = mysql.connection.cursor()
-                mycursor.execute(
-                    "SELECT COUNT(1) FROM hasil_panen WHERE jenis_kopi = %s;", [jeniskopi])
-                if mycursor.fetchone()[0]:
-                    return "Jenis Kopi sudah ada"
-                else:
-                    mycursor.execute(
-                        "INSERT INTO hasil_panen(jenis_kopi, harga, kuantitas) Values(%s,%s,%s)", (jeniskopi, harga, jumlah))
-                    mysql.connection.commit()
-                    mycursor.close()
-                    flash(jeniskopi + ', ' + harga + ', ' +
-                        jumlah + ', Successfully saved!')
-                    return "Success add data"
-            else:
-                return "Method undefined"
-        except Exception as e:
-            flash(e)
-            return JSONEncoder(
-                            data={
-                                "message": "Failed to add data",
-                                "error": e    })
+    if request.method == 'POST':
+        mycursor = mysql.connection.cursor()
+        mycursor.execute(
+            "SELECT COUNT(1) FROM hasil_panen WHERE jenis_kopi = %s;", [jeniskopi])
+        if mycursor.fetchone()[0]:
+            return "Jenis Kopi sudah ada"
+        else:
+            mycursor.execute(
+                "INSERT INTO hasil_panen(jenis_kopi, harga, kuantitas) Values(%s,%s,%s)", (jeniskopi, harga, jumlah))
+            mysql.connection.commit()
+            mycursor.close()
+            flash(jeniskopi + ', ' + harga + ', ' +
+                jumlah + ', Successfully saved!')
+            return "Success add data"
+    else:
+        return "Method undefined"
 
+        # except Exception as e:
+        #     flash(e)
+        #     return JSONEncoder(
+        #                     data={
+        #                         "message": "Failed to add data",
+        #                         "error": e    })
 
 
 @app.route('/pencatatan/search', methods=['POST'])
@@ -150,8 +122,8 @@ def search():
     try:
         jeniskopi = request.form['carikopi']
         if jeniskopi == "":
-            flash('Harap isi kolom pencarian ')
-            return render_template('index.html')
+            # flash('Harap isi kolom pencarian ')
+            return "Harap isi kolom pencarian"
         else:
             if request.method == 'POST':
                 mycursor = mysql.connection.cursor()
@@ -209,48 +181,82 @@ def delete():
 @app.route('/pencatatan/update', methods=['POST'])
 def update():
     try:
-       name_prod = request.form['namess']
-       price = request.form['pricee']
-       quantt = request.form['quantityy']
-       if name_prod == "":
-           return render_template('index.html')
-       elif price == "":
-           return render_template('index.html')
-       elif quantt == "":
-           return render_template('index.html')
-       else:
-           if request.method == 'POST':
-               mycursor = mysql.connection.cursor()
-               if request.method == 'POST':
-                   mycursor.execute(
-                       "SELECT COUNT(1) FROM hasil_panen WHERE product_name = %s;", [name_prod])
-                   if mycursor.fetchone()[0]:
-                       mycursor.execute(
-                           "SELECT * FROM hasil_panen WHERE product_name = %s;", [name_prod])
-                       produ = mycursor.fetchall()
-                       flash(name_prod + ' Ditemukan!')
-                       for row in produ:
-                           b = row[3]
-                           if int(b)+int(quantt) < 0:
-                               flash('Tidak cukup')
-                               return render_template('index.html')
-                           else:
-                               sum = int(b) + int(quantt)
-                               mycursor.execute("UPDATE hasil_panen SET price='" + price + "' , quantity='" + str(
-                                   sum) + "' WHERE product_name='" + name_prod + "'")
-                               mysql.connection.commit()
-                               mycursor.close()
-                               mycursor.close()
-                               flash('Berhasil Diupdate')
-                               return render_template('index.html')
-                   else:
-                       flash('Tidak ada jenis kopi tersebut ' + name_prod)
-                   return render_template('index.html')
+        name_prod = request.form['namess']
+        price = request.form['pricee']
+        quantt = request.form['quantityy']
+        #timee = request.form ['timee']
+        if name_prod == "":
+            return render_template('index.html')
+        elif price == "":
+            return render_template('index.html')
+        elif quantt == "":
+            return render_template('index.html')
+        # elif timee == "":
+        #     return render_template('index.html')
+        else:
+            if request.method == 'POST':
+                mycursor = mysql.connection.cursor()
+                if request.method == 'POST':
+                    mycursor.execute(
+                        "SELECT COUNT(1) FROM hasil_panen WHERE product_name = %s;", [name_prod])
+                    if mycursor.fetchone()[0]:
+                        mycursor.execute(
+                            "SELECT * FROM hasil_panen WHERE product_name = %s;", [name_prod])
+                        produ = mycursor.fetchall()
+                        flash(name_prod + ' Ditemukan!')
+                        for row in produ:
+                            b = row[3]
+                            if int(b)+int(quantt) < 0:
+                                flash('Tidak cukup')
+                                return render_template('index.html')
+                            else:
+                                sum = int(b) + int(quantt)
+                                mycursor.execute("UPDATE hasil_panen SET price='" + price + "' , quantity='" + str(
+                                    sum) + "' WHERE product_name='" + name_prod + "'")
+                                mysql.connection.commit()
+                                mycursor.close()
+                                mycursor.close()
+                                flash('Berhasil Diupdate')
+                                return render_template('index.html')
+                    else:
+                        flash('Tidak ada jenis kopi tersebut ' + name_prod)
+                    return render_template('index.html')
     except Exception as e:
         flash(e)
         return render_template('index.html')
 
-        
+
+# model = keras.models.load_model('model_v1.h5')
+
+
+# class NumpyArrayEncoder(JSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, np.ndarray):
+#             return obj.tolist()
+#         return JSONEncoder.default(self, obj)
+
+
+# def predict_image(path):
+#     img = image.load_img(path, target_size=(224, 224))
+#     x = image.img_to_array(img)
+#     x = np.expand_dims(x, axis=0)
+#     images = np.vstack([x])
+#     classes = model.predict(images, batch_size=32)
+#     return classes
+
+
+# def dictionary(result):
+#     if result == 0:
+#         return {"result": "Healthy}
+#     if result == 1:
+#         return {"result": "Miner"}
+#     if result == 2:
+#         return {"result": "Phoma"}
+#     if result == 3:
+#         return {"result": "Rust"}
+
+
+
 # @app.route("/penyakit", methods=['POST'])
 # def predict():
 #     if request.method == 'POST':
@@ -262,7 +268,7 @@ def update():
 #         updatedAt = datetime.now()
 
 #         splitfile = os.path.splitext(img.filename)
-#         fileName = splitfile[0] + str(random.randint(1,1000)) + splitfile[1]
+#         fileName = splitfile[0] + str(random.randint(1, 1000)) + splitfile[1]
 #         img.save("./static/" + fileName)
 #         url = os.path.join('static/', fileName)
 #         img_path = url
@@ -272,7 +278,8 @@ def update():
 #         result = dictionary(p)
 
 #         cur = mysql.connection.cursor()
-#         cur.execute("INSERT INTO penyakits(indikasi, latitude, longitude, createdAt, updatedAt, image, url) VALUES (%s, %s, %s, %s, %s, %s, %s)", (result['result'], latitude, longitude, createdAt, updatedAt, fileName, url))
+#         cur.execute("INSERT INTO penyakits(indikasi, latitude, longitude, createdAt, updatedAt, image, url) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+#                     (result['result'], latitude, longitude, createdAt, updatedAt, fileName, url))
 #         mysql.connection.commit()
 #         cur.close()
 #         return {
@@ -281,28 +288,29 @@ def update():
 #             "data": result
 #         }
 
-# # @app.route("/penyakit/<int:id_penyakit>", methods=['PUT'])
-# # def update(id_penyakit):
-# #     if request.method == 'PUT':
-# #         cur = mysql.connection.cursor()
-# #         searchpenyakit = cur.execute(
-# #             "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
-# #         row_headers = [x[0] for x in cur.description]
-# #         if (searchpenyakit > 0):
-# #             penyakit = cur.fetchall()
-# #             json_data = []
-# #             for result in penyakit:
-# #                 json_data.append(dict(zip(row_headers, result)))
-# #             # return jsonify(json_data)
-# #             if (request.files['image'].filename == ''):
-# #                 fileName = json_data[0]['image']
-# #             else:
-# #                 img = request.files['image']
-# #                 os.remove("./static/" + json_data[0]['image'])
-# #                 splitfile = os.path.splitext(img.filename)
-# #                 fileName = splitfile[0] + \
-# #                     str(random.randint(1, 1000)) + splitfile[1]
-# #                 img.save("./static/" + fileName)
+
+# @app.route("/penyakit/<int:id_penyakit>", methods=['PUT'])
+# def update(id_penyakit):
+#     if request.method == 'PUT':
+#         cur = mysql.connection.cursor()
+#         searchpenyakit = cur.execute(
+#             "SELECT * FROM penyakits WHERE id_penyakit = {}".format(id_penyakit))
+#         row_headers = [x[0] for x in cur.description]
+#         if (searchpenyakit > 0):
+#             penyakit = cur.fetchall()
+#             json_data = []
+#             for result in penyakit:
+#                 json_data.append(dict(zip(row_headers, result)))
+#             # return jsonify(json_data)
+#             if (request.files['image'].filename == ''):
+#                 fileName = json_data[0]['image']
+#             else:
+#                 img = request.files['image']
+#                 os.remove("./static/" + json_data[0]['image'])
+#                 splitfile = os.path.splitext(img.filename)
+#                 fileName = splitfile[0] + \
+#                     str(random.randint(1, 1000)) + splitfile[1]
+#                 img.save("./static/" + fileName)
 
 #             penyakitDetails = request.form
 #             latitude = penyakitDetails['latitude']
@@ -317,10 +325,10 @@ def update():
 #             print(p)
 #             result = dictionary(p)
 
-##              cur = mysql.connection.cursor()
+#             cur = mysql.connection.cursor()
 #             cur.execute("UPDATE penyakits SET indikasi=%s, latitude=%s, longitude=%s, createdAt=%s, updatedAt=%s, image=%s, url=%s WHERE id_penyakit=%s",
 #                         (result['result'], latitude, longitude, createdAt, updatedAt, fileName, url, id_penyakit))
-##              mysql.connection.commit()
+#             mysql.connection.commit()
 #             cur.close()
 #             return {
 #                 "status": 204,
@@ -332,11 +340,11 @@ def update():
 #             return "penyakit not found"
 
 
-##  @app.route('/penyakit', methods=['GET'])
+# @app.route('/penyakit', methods=['GET'])
 # def get_penyakit():
 #     cur = mysql.connection.cursor()
 #     result = cur.execute("SELECT * FROM penyakits AS result")
-##      row_headers = [x[0] for x in cur.description]
+#     row_headers = [x[0] for x in cur.description]
 #     if result > 0:
 #         penyakitDetails = cur.fetchall()
 #         json_data = []
